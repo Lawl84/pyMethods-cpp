55df87fde4617c3ba1ffcb7c739e315d7ee664d6
Sid <71117904+epicgamerXDLOL@users.noreply.github.com>
2021-02-04 13:35:33 -0700

    added startswith and endswith

--- pyMethods/pyStr.h
+++ pyMethods/pyStr.h
@@ -1,48 +1,21 @@
 #pragma once
-
 #include <string>
 #include <vector>
-#include <sstream>
 using namespace std;
-
-template<typename T>
-class PyVector
-{
-public:
-
-	vector<T> vecInput;
-
-	PyVector(vector<T>& other)
-	{
-		vecInput = other;
-	}
-
-	string join(string joiner)
-	{
-		string output;
-		for (T& i : vecInput)
-		{
-			stringstream ss;
-			ss << i;
-			output += ss.str();
-			output += joiner;
-		}
-		return output.substr(0, output.length() - joiner.length());
-	}
-};
 
 class PyString
 {
-public:
 	string word;
-	PyString(string& other)
+public:
+
+	PyString(string other)
 	{
 		word = other;
 	}
 
-	
+
 	vector<string> split(string delim)
-	{	
+	{
 		vector<string> splitList;
 		size_t rn = 0, prev = 0;
 		while (rn < word.length() && prev < word.length())
@@ -59,10 +32,20 @@ public:
 
 		return splitList;
 
+	}
+		
+	bool startswith(string str)
+	{
+		return word.substr(0, str.length()) == str;
+	}
 
-
+	bool endswith(string str)
+	{
+		return word.substr(word.length() - str.length(), word.length()) == str;
 	}
 
 
-};
+	
 
+
+};
---
+++ pyMethods/pyVec.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+template<typename T>
+class PyVector
+{
+	vector<T> vecInput;
+public:
+
+	
+
+	PyVector(vector<T>other)
+	{
+		vecInput = other;
+	}
+
+	string join(string joiner)
+	{
+		string output;
+		for (T& i : vecInput)
+		{
+			stringstream ss;
+			ss << i;
+			output += ss.str();
+			output += joiner;
+		}
+		return output.substr(0, output.length() - joiner.length());
+	}
+};
+
+
+
